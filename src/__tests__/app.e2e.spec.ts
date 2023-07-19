26f29ee8e0bf0e1318c25a67d24c96cb141aeab8
forato <lucas7forato@gmail.com>
2023-07-19 20:26:44 -0300

    feat: finished unit test

--- src/__tests__/app.e2e.spec.ts
+++ src/__tests__/app.e2e.spec.ts
@@ -15,10 +15,36 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('(GET) /api/v1/pokemon/ditto', () => {
     return request(app.getHttpServer())
       .get('/api/v1/pokemon/ditto')
       .expect(200)
       .expect(JSON.stringify(['limber', 'imposter']));
+  });
+
+  it('(GET) /api/v1/pokemon/incorrect_name', () => {
+    return request(app.getHttpServer())
+      .get('/api/v1/pokemon/incorrect_name')
+      .expect(500)
+      .expect('Failed to fetch data from PokeAPI');
+  });
+
+  it('(GET) /api/v1/pokemon/pikachu', () => {
+    return request(app.getHttpServer())
+      .get('/api/v1/pokemon/pikachu')
+      .expect(200)
+      .expect(JSON.stringify(['static', 'lightning-rod']));
+  });
+
+  it('(POST) /api/v1/pokemon/ditto', () => {
+    return request(app.getHttpServer())
+      .post('/api/v1/pokemon/ditto')
+      .expect(404);
+  });
+
+  it('(DELETE) /api/v1/pokemon/ditto', () => {
+    return request(app.getHttpServer())
+      .delete('/api/v1/pokemon/ditto')
+      .expect(404);
   });
 });
--- src/app.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { PokeApiAbility } from './infrastructure/types/PokeApiAbility';
-
-@Injectable()
-export class AppService {
-  async filterAbilityNameFromPokemonData(data: PokeApiAbility[]) {
-    return data.map(({ ability }) => ability.name);
-  }
-}
