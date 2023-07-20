83634c043b8d130e0c6c59da543a7ec4e7d25472
Forato <lucas7forato@gmail.com>
2023-07-20 01:11:49 -0300

    feat: added some more unit tests

--- src/__tests__/app.service.unit.spec.ts
+++ src/__tests__/app.service.unit.spec.ts
@@ -9,7 +9,7 @@ describe('App Service Tests', () => {
     appService = new AppService();
   });
 
-  it('should return a list of names', async () => {
+  it('should return a list of names with 2 names', async () => {
     const names = [faker.lorem.word(), faker.lorem.word()];
 
     const data: PokeApiAbility[] = [
@@ -30,6 +30,70 @@ describe('App Service Tests', () => {
         slot: 1,
       },
     ];
+    const response = await appService.filterAbilityNameFromPokemonData(data);
+    expect(response).toEqual(names);
+  });
+
+  it('should return a list of names with 5 names', async () => {
+    const names = [
+      faker.lorem.word(),
+      faker.lorem.word(),
+      faker.lorem.word(),
+      faker.lorem.word(),
+      faker.lorem.word(),
+    ];
+
+    const data: PokeApiAbility[] = [
+      {
+        ability: {
+          name: names[0],
+          url: faker.internet.url(),
+        },
+        is_hidden: false,
+        slot: 1,
+      },
+      {
+        ability: {
+          name: names[1],
+          url: faker.internet.url(),
+        },
+        is_hidden: false,
+        slot: 1,
+      },
+      {
+        ability: {
+          name: names[2],
+          url: faker.internet.url(),
+        },
+        is_hidden: false,
+        slot: 1,
+      },
+      {
+        ability: {
+          name: names[3],
+          url: faker.internet.url(),
+        },
+        is_hidden: false,
+        slot: 1,
+      },
+      {
+        ability: {
+          name: names[4],
+          url: faker.internet.url(),
+        },
+        is_hidden: false,
+        slot: 1,
+      },
+    ];
+    const response = await appService.filterAbilityNameFromPokemonData(data);
+    expect(response).toEqual(names);
+  });
+
+  it('should return an empty list', async () => {
+    const names = [];
+
+    const data: PokeApiAbility[] = [];
+
     const response = await appService.filterAbilityNameFromPokemonData(data);
     expect(response).toEqual(names);
   });
